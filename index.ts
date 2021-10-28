864c66fce717ab20a6b5c35905508f20c63e72ec
Juan Pablo Pardal <juan.pardal@despegar.com>
2021-10-27 22:55:14 -0300

    fix import

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { JazeeraApp } from "./src/lib/jazeera";
-import { Controller, ReqParam, Middleware, OnError, Delete, Get, Options, Post, Put } from "./src/lib/decorators";
+import { Controller, ReqParam, Middleware, OnError, Delete, Get, Options, Post, Put, Patch, Module } from "./src/lib/decorators";
 
 export {
     JazeeraApp,
     Controller,
+    Module,
     ReqParam,
     Middleware,
     OnError,
@@ -11,5 +12,6 @@ export {
     Get,
     Options,
     Post,
-    Put
+    Put,
+    Patch
 }
