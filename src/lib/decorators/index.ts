136fc6d908bfda074773701f2c078b87ea24666c
Juan Pablo Pardal <juan.pardal@despegar.com>
2021-10-27 22:32:23 -0300

    add patch

--- src/lib/decorators/index.ts
+++ src/lib/decorators/index.ts
@@ -1,7 +1,7 @@
 import { Controller, Service } from "./class.decorator";
 import { Middleware, OnError } from "./middleware.decorator";
 import { ReqParam } from "./RouteParams.decorator";
-import { Delete, Get, Options, Post, Put } from "./RequestMethod.decorator";
+import { Delete, Get, Options, Post, Put, Patch } from "./RequestMethod.decorator";
 import { Module } from "./Module.decorator";
 
 export  {
@@ -15,5 +15,6 @@ export  {
     Post,
     Delete,
     Options,
-    Put
+    Put,
+    Patch
 }
